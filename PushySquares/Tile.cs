bb872a99a857465c00ef4ceef7b999b83bbb12c2
Sweeper777 <sumulang@gmail.com>
2017-11-19 10:00:11 +0000

    added TileExtensions

--- PushySquares/Tile.cs
+++ PushySquares/Tile.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace PushySquares {
 	public enum Color {
@@ -11,5 +11,24 @@ namespace PushySquares {
 
 	public enum Tile {
 		Empty, Void, Wall, SquareColor1, SquareColor2, SquareColor3, SquareColor4, SquareGrey
+	}
+
+	public static class TileExtensions {
+		public static Tile FromColor(Color color) {
+			switch (color) {
+			case Color.Color1:
+				return Tile.SquareColor1;
+			case Color.Color2:
+				return Tile.SquareColor2;
+			case Color.Color3:
+				return Tile.SquareColor3;
+			case Color.Color4:
+				return Tile.SquareColor4;
+			case Color.Grey:
+				return Tile.SquareGrey;
+			default:
+				throw new ArgumentException ("Invalid Color!");
+			}
+		}
 	}
 }
