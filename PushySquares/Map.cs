19a8753ba3ff2e2208d9480b0b4951653f62b0b6
Sweeper777 <sumulang@gmail.com>
2017-11-21 18:50:41 +0000

    added Map constructor

--- PushySquares/Map.cs
+++ PushySquares/Map.cs
@@ -6,5 +6,10 @@ namespace PushySquares {
 	public struct Map {
 		public Tile[,] Board { get; }
 		public Dictionary<Color, Position> Spawnpoints { get; set; }
+		public Map(Tile[,] board, Dictionary<Color, Position> spawnpoints) {
+			Board = board;
+			Spawnpoints = spawnpoints;
+		}
+
 	}
 }
