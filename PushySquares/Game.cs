b540302ca76612c387e35a3ac3a78c3a92eb4caa
Sweeper777 <sumulang@gmail.com>
2017-11-21 18:53:15 +0000

    added PlayerCountToTurnsUntilNewSquare

--- PushySquares/Game.cs
+++ PushySquares/Game.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 
 namespace PushySquares {
 	public class Game {
+		public static readonly Dictionary<int, int> PlayerCountToTurnsUntilNewSquare = new Dictionary<int, int>() {
+			{ 2, 2 },
+			{ 3, 4 },
+			{ 4, 4 }
+		};
+
 	}
 
 	public delegate void GameDelegate(Direction? direction, 
