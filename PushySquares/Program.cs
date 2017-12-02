2b4cb340de33df4fc4fc080fa15aca4075fda7b3
Sweeper777 <sumulang@gmail.com>
2017-12-02 08:24:25 +0000

    tested game

--- PushySquares/Program.cs
+++ PushySquares/Program.cs
@@ -3,9 +3,11 @@ using System.Text;
 using System.Linq;
 
 namespace PushySquares {
-	class MainClass {
-		public static void Main (string[] args) {
-            
+    class MainClass {
+        public static void Main(string[] args) {
+            //Game game = new Game(Map.Standard, 4);
+            //Console.WriteLine(game);
+            //game.MoveDown();
+            //Console.WriteLine(game);
         }
-	}
 }
