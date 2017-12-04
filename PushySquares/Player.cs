c57a9ffef3c3833bf677ef9fe3955c7e57e941e3
Sweeper777 <sumulang@gmail.com>
2017-12-04 19:26:58 +0000

    added CreateCopy method in Player

--- PushySquares/Player.cs
+++ PushySquares/Player.cs
@@ -2,7 +2,7 @@
 
 namespace PushySquares {
 
-	public class Player {
+    public class Player {
 		public int TurnsUntilNewSquare { get; set; }
 		private int lives;
 		public int Lives {
@@ -16,6 +16,10 @@ namespace PushySquares {
 			TurnsUntilNewSquare = turnsUntilNewSquare;
 			Color = color;
 		}
-	}
+
+        public Player CreateCopy() {
+            return new Player(TurnsUntilNewSquare, Lives, Color);
+        }
+    }
 }
 
