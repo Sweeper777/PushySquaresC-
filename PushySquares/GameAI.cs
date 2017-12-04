01bea59109b210e2aeda2d77478cbdfea7a7b95b
Sweeper777 <sumulang@gmail.com>
2017-12-04 19:28:07 +0000

    added GetPlayer extension

--- PushySquares/GameAI.cs
+++ PushySquares/GameAI.cs
@@ -2,4 +2,10 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 namespace PushySquares {
+    public static class GameExtensions {
+        public static Player GetPlayer(this Game game, Color color) {
+            return game.Players.Where(x => x.Color == color).First();
+        }
+
+    }
 }
