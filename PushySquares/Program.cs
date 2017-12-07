8c743f715a21dcb40c22863c4f17a878efc4b898
Sweeper777 <sumulang@gmail.com>
2017-12-07 07:09:45 +0000

    added PlayWIthAI

--- PushySquares/Program.cs
+++ PushySquares/Program.cs
@@ -36,4 +36,48 @@ namespace PushySquares {
                 Console.WriteLine(game);
             }
         }
+
+        public static void PlayWithAI()
+        {
+            Game game = new Game(Map.Standard, 2);
+            Console.WriteLine(game);
+            while (game.Players.Where(x => x.Lives > 0).Count() > 1)
+            {
+                if (game.CurrentPlayer.Color == Color.Color1)
+                {
+                    var direction = Console.ReadKey();
+                    switch (direction.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            game.MoveUp();
+                            break;
+                        case ConsoleKey.DownArrow:
+                            game.MoveDown();
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            game.MoveLeft();
+                            break;
+                        case ConsoleKey.RightArrow:
+                            game.MoveRight();
+                            break;
+                    }
+                }
+                else
+                {
+                    var ai = new GameAI(game.CreateCopy(), game.CurrentPlayer.Color, 553, 8371, 3, 5646, 3791, 8583, 6187, 680, 9157);
+                    game.Move(ai.NextMove());
+                    //var right = game.CreateCopy();
+                    //right.MoveRight();
+                    //var ai = new GameAI(right.CreateCopy(), game.CurrentPlayer.Color, 553, 8371, 3, 5646, 3791, 8583, 6187, 680, 9157);
+                    //Console.WriteLine(ai.EvaluateHeuristics());
+                    //var left = game.CreateCopy();
+                    //left.MoveLeft();
+                    //ai = new GameAI(left.CreateCopy(), game.CurrentPlayer.Color, 553, 8371, 3, 5646, 3791, 8583, 6187, 680, 9157);
+                    //Console.WriteLine(ai.EvaluateHeuristics());
+                    //break;
+                }
+                Console.WriteLine(game);
+            }
+        }
+
 }
