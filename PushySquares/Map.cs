b713f18d4b002997811ed2efe564c8938486236d
Sweeper777 <sumulang@gmail.com>
2017-12-09 08:06:33 +0000

    documented Map.cs

--- PushySquares/Map.cs
+++ PushySquares/Map.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace PushySquares {
+    /// <summary>
+    /// Represnets an arrangement of holes, walls, initial squares and spawnpoints used in a game of 
+    /// PushySquares.
+    /// </summary>
 	public struct Map {
+        /// <summary>
+        /// Gets the game board.
+        /// </summary>
+        /// <value>The game board.</value>
 		public Tile[,] Board { get; }
+
+        /// <summary>
+        ///  Gets or sets the spawnpoints.
+        /// </summary>
+        /// <value>The spawnpoints.</value>
 		public Dictionary<Color, Position> Spawnpoints { get; set; }
 
+        /// <summary>
+        /// The standard map used in a standard game of PushySquares.
+        /// </summary>
 		public static readonly Map Standard = new Map(
 @"....OO....
 .1++++++2.
@@ -19,11 +35,57 @@ O+++OO+++O
 .4++++++3.
 ....OO....");
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:PushySquares.Map"/> struct with the specifed
+        /// board configuration and spawnpoints.
+        /// </summary>
+        /// <param name="board">The initial board configuration.</param>
+        /// <param name="spawnpoints">The spawnpoints.</param>
 		public Map(Tile[,] board, Dictionary<Color, Position> spawnpoints) {
 			Board = board;
 			Spawnpoints = spawnpoints;
 		}
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:PushySquares.Map"/> struct with a string
+        /// representing the initial map configuration and spawnpoints
+        /// </summary>
+        /// <param name="mapString">The string representing the initial map configuration and
+        /// spawnpoints</param>
+        /// <remarks>
+        /// The map string must be a matrix of characters, with new rows denoted by new line 
+        /// characters. This table shows what character corresponds to what <see cref="T:PushySquares.Tile"/>.
+        /// <list type="table">
+        ///     <item>
+        ///         <item><code>.</code></item>
+        ///         <item><see cref="E:PushySquares.Tile.Void"/></item>
+        ///     </item>
+        ///     <item>
+        ///         <item><code>+</code></item>
+        ///         <item><see cref="E:PushySquares.Tile.Empty"/></item>
+        ///     </item>
+        ///      <item>
+        ///         <item><code>O</code></item>
+        ///         <item><see cref="E:PushySquares.Tile.Wall"/></item>
+        ///     </item>
+        ///      <item>
+        ///         <item><code>1</code></item>
+        ///         <item>Spawnpoint for <see cref="E:PushySquares.Color.Color1"/></item>
+        ///     </item>
+        ///      <item>
+        ///         <item><code>2</code></item>
+        ///         <item>Spawnpoint for <see cref="E:PushySquares.Color.Color2"/></item>
+        ///     </item>
+        ///      <item>
+        ///         <item><code>3</code></item>
+        ///         <item>Spawnpoint for <see cref="E:PushySquares.Color.Color3"/></item>
+        ///     </item>
+        ///      <item>
+        ///         <item><code>4</code></item>
+        ///         <item>Spawnpoint for <see cref="E:PushySquares.Color.Color4"/></item>
+        ///     </item>
+        /// </list>
+        /// </remarks>
 		public Map(string mapString) {
 			string[] lines = mapString.Split(Environment.NewLine.ToCharArray()).Where(x => x != "").ToArray();
 			Board = new Tile[lines[0].Length, lines.Length];
